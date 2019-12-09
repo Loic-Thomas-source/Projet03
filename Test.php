a11521709c9cf9255b3669f66c326ff62cdf93ec
Loic-Thomas-source <58432521+Loic-Thomas-source@users.noreply.github.com>
2019-12-09 09:05:48 +0100

    Conversion
    Conversion h2,h3 en p
    Mise en place de commentaire
    Changement logo protect
    Update style.css

--- Test.php
+++ Test.php
@@ -8,60 +8,63 @@
     
 	<div ID="logo">
     <p>
-            <a href="logo.jpg"><img src="logo_mini.jpg" alt="" /></a>
+            <a href="logo.jpg"><img src="logo_mini.jpg" alt="" /></a> <!--Logo gbaf -->
     </p>
     </div>
 
     
-    <hr>
+    <hr> <!-- Ligne de séparation -->
+
 </header>
 <body>
 	<section>
-    <h1>Le GBAF</h1>
-    <h1>Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
+    <h1>LE GBAF</h1>
+    <center><p>Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
             les axes de la réglementation financière française. Sa mission est de promouvoir
             l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des
             pouvoirs publics.<br>Pour remédier à cela, le GBAF souhaite proposer aux salariés des grands groupes
             français un point d’entrée unique, répertoriant un grand nombre d’informations
             sur les partenaires et acteurs du groupe ainsi que sur les produits et services
             bancaires et financiers.
-            Chaque salarié pourra ainsi poster un commentaire et donner son avis.</h1>
+            Chaque salarié pourra ainsi poster un commentaire et donner son avis.</p></center>
         <p>
-            <img src = "image.jpg" width="1890px" height="300px" alt= "illustration" />
+            <img src = "image.jpg" width="1890px" height="300px" alt= "illustration" /> <!-- Photo illustration -->
         </p>
 
 	
 
-    <hr>
-    <h2>
+    <hr> <!-- Ligne de séparation -->
+    
         <h2>
             Nos acteurs partenaires : 
         <h2>
+        <p>    
         <ul>
             <li>Formation&co est une association française présente sur tout le territoire.</li>
             <li>Protectpeople finance la solidarité nationale.</li>
             <li>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.</li>
             <li>La CDE accompagne les entreprises dans leurs démarches de formation. </li>
         </ul>
-	</h2>
+        </p>
+	
     </section>
     
     <section class = "grille">
-        <article> <img src = "protectpeople1.png" alt="" /> <p> <a href = "protectpeople.html">lire la suite </a></p>  <h3> Protectpeople finance la solidarité nationale.
+        <article> <img src = "protectpeople1.png" alt="" /> <p> <a href = "protectpeople.html">lire la suite </a></p>  <p> Protectpeople finance la solidarité nationale.
                 Nous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de 
-                bénéficier d’une protection sociale.</h3> </article>
-        <article> <img src = "CDE1.png" alt="" /> <p> <a href = "CDE.html">Lire la suite </a></p> <h3> La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans
+                bénéficier d’une protection sociale.</p> </article>
+        <article> <img src = "CDE1.png" alt="" /> <p> <a href = "CDE.html">Lire la suite </a></p> <p> La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans
              leurs démarches de formation. 
                 Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.
-                </h3></article>
-        <article> <img src = "Dsa_france1.png" alt="" /> <p> <a href = "DSA.html">Lire la suite</a></p> <h3> Dsa France accélère la croissance du territoire et s’engage 
+                </p></article>
+        <article> <img src = "Dsa_france1.png" alt="" /> <p> <a href = "DSA.html">Lire la suite</a></p> <p> Dsa France accélère la croissance du territoire et s’engage 
             avec les collectivités territoriales.
                 Nous accompagnons les entreprises dans les étapes clés de leur évolution.<br/>
-                Notre philosophie : s’adapter à chaque entreprise.</h3></article>
-        <article> <img src = "formationetco1.png" alt="" /> <p> <a href = "formation.html">Lire la suite</a></p> <h3> Formation&co est une association française présente sur
+                Notre philosophie : s’adapter à chaque entreprise.</p></article>
+        <article> <img src = "formationetco1.png" alt="" /> <p> <a href = "formation.html">Lire la suite</a></p> <p> Formation&co est une association française présente sur
              tout le territoire.
                 Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit<br/> et un accompagnement
-                 professionnel et personnalisé.</h3></article>
+                 professionnel et personnalisé.</p></article>
 
              
     </section>
